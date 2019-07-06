a09e70ebf7e5756b3ab3f48a8937177f477691e6
Ryan Festag <rfestag@gmail.com>
2019-07-06 10:49:15 -0600

    Still trying to package properly

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "dist/"),
     filename: "index.js",
     library: "",
     libraryTarget: "commonjs"
