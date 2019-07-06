7ae86e73d0b793e4c89e20258e5a5e88a4ee0507
Ryan Festag <rfestag@gmail.com>
2019-07-06 10:28:32 -0600

    Trying to remove references to dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    //path: path.resolve(__dirname, "./dist"),
     path: __dirname,
     filename: "index.js",
     library: "",
