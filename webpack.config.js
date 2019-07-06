629ffee0b7825615ede04e92927eeb9210a9a59d
Ryan Festag <rfestag@gmail.com>
2019-07-06 08:37:52 -0600

    Using webpack for building library, parcel for testing locally

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    library: "",
+    libraryTarget: "commonjs"
+  },
+  externals: {
+    react: "commonjs react"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|tutorials)/,
+        loader: "babel-loader",
+        resolve: { extensions: [".js", ".jsx"] },
+        options: {
+          presets: ["@babel/preset-env", "@babel/react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
