5b890f90572168b4eac6eb5857815a4046c0410a
Ryan Festag <rfestag@gmail.com>
2019-07-05 18:04:15 -0600

    Trying to reduce cognitive complexity

--- src/components/Collapsed.jsx
+++ src/components/Collapsed.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { More } from "./More";
+import { simpleObject } from "../utils/common";
 
-export const Collapsed = ({ name, value }) => {
-  if (value instanceof RegExp || value instanceof Date) {
-    return (
-      <span>
-        {name && `${name} : `}
-        {value.toString()}
-      </span>
-    );
-  } else {
-    const open = value instanceof Array ? "[" : "{";
-    const close = value instanceof Array ? "]" : "}";
-    return (
-      <span>
-        {name && `${name} : `}
-        {open}
-        <span>
-          <More />
-        </span>
-        {close}
-      </span>
-    );
-  }
-};
-Collapsed.propTypes = {
+const collapsedProperties = {
   name: PropTypes.string,
   value: PropTypes.any
 };
+
+const CollapsedSimple = ({ name, value }) => (
+  <span>
+    {name && `${name} : `}
+    {value.toString()}
+  </span>
+);
+CollapsedSimple.propTypes = collapsedProperties;
+
+const CollapsedComplex = ({ name, value }) => {
+  const open = value instanceof Array ? "[" : "{";
+  const close = value instanceof Array ? "]" : "}";
+  return (
+    <span>
+      {name && `${name} : `}
+      {open}
+      <span>
+        <More />
+      </span>
+      {close}
+    </span>
+  );
+};
+CollapsedComplex.propTypes = collapsedProperties;
+
+export const Collapsed = ({ name, value }) =>
+  simpleObject(value) ? (
+    <CollapsedSimple name={name} value={value} />
+  ) : (
+    <CollapsedComplex name={name} value={value} />
+  );
+Collapsed.propTypes = collapsedProperties;
